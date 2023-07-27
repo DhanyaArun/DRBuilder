2f4520ef1487b37e50811eb31a8f4cff71b84eae
DhanyaArun <97540964+DhanyaArun@users.noreply.github.com>
2023-07-27 13:09:23 +0530

    Updated Testclass
    test class

--- src/test/java/org/drb/scripts/TestClass.java
+++ src/test/java/org/drb/scripts/TestClass.java
@@ -6,6 +6,7 @@ import org.drb.pages.LoginPage;
 import org.drb.utilities.ExcelUtility;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 
@@ -13,6 +14,13 @@ import org.testng.annotations.Test;
 public class TestClass extends TestBase
 {
   LoginPage log;
+  
+  
+  @AfterMethod
+  public void afterMethod() 
+  {
+	  driver.quit();
+  }
 
 
 	@Test
@@ -26,10 +34,12 @@ public class TestClass extends TestBase
 	}
 
 	
+	//Testing with valid username and password by ticking the Remember me checkbox
 	
 	@Test
 	public void  verifyValidLogin() throws IOException
 	{
+		//Dhanya
 	log=new LoginPage(driver);
 	String Username=ExcelUtility.getData(0, 0);
 	String Password=ExcelUtility.getData(0, 1);
@@ -38,38 +48,44 @@ public class TestClass extends TestBase
 	log.setPassword(Password);
 	log.RememberClick();
 	log.LoginClick();
+	//log.LogoutClick();
+	//log.AboutUsClick();
+	//log.TemplateClick();
+	//log.MyProfile();
 	//log.createResume();
-	String actualresult=log.getLogoText();
-	System.out.println(actualresult);
-	Assert.assertEquals(actualresult,"http://167.71.226.96/login");
+	//String actualresult=log.getLogoText();
+	//System.out.println(actualresult);
+	//Assert.assertEquals(actualresult,"http://167.71.226.96/login");
 	
 	
 }
-	
+	//Testing with valid username and password without ticking the Remember me checkbox
 
-	//@Test
-	/*public void verifyInvalidUsername() throws IOException
+	@Test
+	public void verifyUncheckRemember() throws IOException
 	{
 		log=new LoginPage(driver);
-		String Username1=ExcelUtility.getData(1, 0);
-		String Password1=ExcelUtility.getData(1,1);
+		String Username1=ExcelUtility.getData(0, 0);
+		String Password1=ExcelUtility.getData(0,1);
 		log.SignupClick();
 		log.setUsername(Username1);
 		log.setPassword(Password1);
-		log.RememberClick();
 		log.LoginClick();	
-		String actalresult=log.getLogoText();
-		Assert.assertEquals(actalresult, "LOGOUT");
+		String actualresult=log.getLogoText();
+		Assert.assertEquals(actualresult, "LOGOUT");
 		
 	}
-	*/
-	//@Test
-	/*public void verifyInvalidPassword() throws IOException
+	
+	
+	//Testing with invalid username and valid password
+	
+	@Test
+	public void verifyInvalidUsername() throws IOException
 	{
 		log=new LoginPage(driver);
 		log.SignupClick();
-		String Username1=ExcelUtility.getData(2,0);
-		String Password1=ExcelUtility.getData(2,2);
+		String Username1=ExcelUtility.getData(1,0);
+		String Password1=ExcelUtility.getData(1,1);
 	
 		log.setUsername(Username1);
 		log.setPassword(Password1);
@@ -79,18 +95,23 @@ public class TestClass extends TestBase
 		Assert.assertEquals(actualresult, "LOGOUT");
 	}
 	
-	*/
-	//@Test
-	/*public void verifyCreateResume() throws IOException
+	
+	//Testing with invalid username and invalid password
+	 
+		 
+	 @Test
+	 
+	public void verifyInvalid() throws IOException
+	
 	{
 		log=new LoginPage(driver);
-		String Username=ExcelUtility.getData(0, 0);
-		String Password=ExcelUtility.getData(0, 1);
+		String Username=ExcelUtility.getData(2, 0);
+		String Password=ExcelUtility.getData(2, 2);
 		log.SignupClick();
 		log.setUsername(Username);
 		log.setPassword(Password);
 		log.RememberClick();
 		log.LoginClick();
-		log.createResume();
-	}*/
+	
+	}
 }
